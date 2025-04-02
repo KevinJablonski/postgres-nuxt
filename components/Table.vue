31cd3f0b813724688255fb62b56452514ff4688f
KevinJablonski <113209802+KevinJablonski@users.noreply.github.com>
2025-04-02 01:25:53 +0000

    Logs added

--- components/Table.vue
+++ components/Table.vue
@@ -58,4 +58,5 @@ export default {
     },
   },
 }
+console.log("Components", stylists,duration)
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@
       class="pt-4 pb-8 bg-gradient-to-br dark:from-white from-black via-[#707070] to-[#ffffff] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
     >
       Salon Stylists
+
     </h1>
     <Table :stylists="data?.stylists" :duration="data?.duration" />
   
@@ -14,4 +15,5 @@
 
 <script setup>
 const { data } = await useFetch('/api/get-users')
+console.log("Data:",data)
 </script>
